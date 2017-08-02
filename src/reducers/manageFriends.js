4088cefb92f11d52aeb86ea6f4b2aba49bda5528
Brett Schules <brettschules@gmail.com>
2017-08-02 12:25:33 -0400

    Done

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,14 @@
 export function manageFriends(state, action){
+  switch (action.type) {
+    case 'ADD_FRIEND':
+      return {friends:[...state.friends,action.friend]}
+    case 'REMOVE_FRIEND':
+
+      const removeFriendId = action.id
+      const arrayFriends = state.friends.filter(friend => friend.id !== action.id)
+      return state = {friends: arrayFriends}
+
+    default:
+        return state;
+  }
 }
